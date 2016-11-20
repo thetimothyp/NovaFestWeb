62f0b6801aee50f8647d7e68209e153f85e4156b
Tim Park <timothsp@uci.edu>
2016-11-20 14:16:45 -0800

    Minor text fixes

--- app/scripts/grid.js
+++ app/scripts/grid.js
@@ -31,18 +31,33 @@ function getTrap(imgURL) {
 
 var traps = [
 	{
-		name : 'POOP',
-		imgURL : 'images/traps/poop.png',
-		desc : 'An interstellar poop with an appetite for flesh, this \
-			radioactive turd kills players on contact.',
-		vidURL : 'images/bg.mp4'
-	},
-	{
 		name : 'PLACEHOLDER',
 		imgURL : 'images/player.png',
 		desc : 'REPLACE THIS STUFF',
 		vidURL : 'images/bg.mp4'
 	},
+	////////////////
+	// STATIC TRAPS
+	////////////////
+	{
+		name : 'PLOO',
+		imgURL : 'images/traps/poop.png',
+		desc : 'A blue interstellar poop with an appetite for flesh, this \
+			radioactive turd kills players on contact.',
+		vidURL : 'images/bg.mp4'
+	},
+	{
+		name : 'MINITHULU',
+		imgURL : 'images/traps/jelly.png',
+		desc : 'These miniature acolytes of the Great Mechathulu are deadlier \
+			than they look; those who touch these will meet a shocking end.',
+		vidURL : 'images/bg.mp4'
+	},
+	// PUT TESLA COIL HERE
+
+	////////////////////////
+	// REPEATING TRAPS
+	////////////////////////
 	{
 		name : 'STAR CANNON',
 		imgURL : 'images/traps/star_cannon.png',
@@ -50,21 +65,55 @@ var traps = [
 		vidURL : 'images/bg.mp4'
 	},
 	{
+		name : 'KITTY KANNON',
+		imgURL : 'images/traps/kitty.png',
+		desc : 'This is a Kitty Kannon.',
+		vidURL : 'images/bg.mp4'
+	},
+	// PUT FLAMETHROWER HERE
+
+	////////////////////////
+	// ENVIRONMENTAL TRAPS
+	////////////////////////
+	{
 		name : 'TELEPORTER',
 		imgURL : 'images/traps/teleporter.png',
-		desc : 'This is a Teleporter.',
+		desc : 'A godsend for lazy tourists the universe over, nobody \
+			really knows how this teleporter works. But it\'s mostly safe to use.',
 		vidURL : 'images/trap_videos/teleporter.mp4'
 	},
 	{
 		name : 'ICE PATCH',
 		imgURL : 'images/traps/ice.png',
-		desc : 'This is an Ice Patch.',
+		desc : 'This glossy substance has a curiously low coefficient of \
+			friction. One might just slide right over it...',
 		vidURL : 'images/trap_videos/ice.mp4'
 	},
+	{
+		name : 'HELL\'S WINDOW',
+		imgURL : 'images/traps/hellsgate.png',
+		desc : 'Take a peek into Hell\'s Window and you\'re off in a \
+			blaze of glory. You\'ll lose basic motor function, but you\'ll \
+			be fast.',
+		vidURL : 'images/trap_videos/hell.mp4'
+	},
+
+	/////////////////
+	// GLOBAL TRAPS
+	/////////////////
 	{
 		name : 'THE GHOSTIES',
 		imgURL : 'images/traps/ghosties.png',
 		desc : 'This is a Ghostie.',
 		vidURL : 'images/bg.mp4'
+	},
+	{
+		name : 'COSMIC FLATULENCE',
+		imgURL : 'images/traps/creep.png',
+		desc : 'This cosmic gas has welcomed more than a few interstellar \
+			travelers into its toxic embrace. Don\'t touch it.',
+		vidURL : 'images/bg.mp4'
 	}
+
+	// PUT CENTRAL FLAMETHROWER HERE
 ];
